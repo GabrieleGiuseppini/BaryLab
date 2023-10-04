2dddf73672f1d46dbc8abc3be7dfceab12185ca9
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2023-10-04 22:07:23 +0200

    Updated plan

--- BLabCoreLib/LabParameters.cpp
+++ BLabCoreLib/LabParameters.cpp
@@ -9,7 +9,7 @@ LabParameters::LabParameters()
 	: MassAdjustment(1.0f)
 	, GravityAdjustment(1.0f)
 	, Elasticity(0.6f)
-	, StaticFriction(0.35f)
+	, StaticFriction(0.15f)
 	, KineticFriction(0.031f)
 {
 }
