c9cee1f320457d11d5dabdbb52e0cd4adcd95ec3
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2024-07-22 18:52:07 +0200

    GameGeometry port

--- GameCore/GameGeometry.h
+++ GameCore/GameGeometry.h
@@ -61,13 +61,28 @@ public:
         vec2f const & segmentP2,
         vec2f const & point)
     {
+        return std::sqrt(
+            SquareDistanceToPoint(
+                segmentP1,
+                segmentP2,
+                point));
+    }
+
+    /*
+     * Returns the distance between a point and a segment.
+     */
+    inline static float SquareDistanceToPoint(
+        vec2f const & segmentP1,
+        vec2f const & segmentP2,
+        vec2f const & point)
+    {
         // From https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment
 
         float const segmentSquaredLength = (segmentP2 - segmentP1).squareLength();
         if (segmentSquaredLength == 0.0f)
         {
             // Overlapping endpoints
-            return (segmentP2 - point).length();
+            return (segmentP2 - point).squareLength();
         }
 
         // Consider the line extending the segment, parameterized as P1 + t (P2 - P1).
@@ -76,7 +91,7 @@ public:
         // We clamp t from [0,1] to handle points outside the segment P1-P2.
         const float t = std::max(0.0f, std::min(1.0f, (point - segmentP1).dot(segmentP2 - segmentP1) / segmentSquaredLength));
         vec2f const projection = segmentP1 + (segmentP2 - segmentP1) * t;  // Projection falls on the segment
-        return (projection - point).length();
+        return (projection - point).squareLength();
     }
 };
 
@@ -129,3 +144,125 @@ inline bool IsPointInTriangle(
         && (pPosition - cPosition).cross(aPosition - cPosition) >= 0.0f;
 }
 
+/*
+ * Calculates a line path between (and including) the specified endpoints, going
+ * through integral coordinates.
+ */
+
+enum class IntegralLineType
+{
+    Minimal,
+    WithAdjacentSteps
+};
+
+template<IntegralLineType TType, typename _TIntegralTag, typename TVisitor>
+inline void GenerateIntegralLinePath(
+    _IntegralCoordinates<_TIntegralTag> const & startPoint,
+    _IntegralCoordinates<_TIntegralTag> const & endPoint,
+    TVisitor const & visitor)
+{
+    //
+    // Visit starting point
+    //
+
+    visitor(startPoint);
+
+    // Check whether we are done
+    if (startPoint == endPoint)
+    {
+        return;
+    }
+
+    //
+    // "Draw" line from start position to end position
+    //
+    // Go along widest of Dx and Dy, in steps of 1.0, until we're very close to end position
+    //
+
+    // W = wide, N = narrow
+
+    int const dx = endPoint.x - startPoint.x;
+    int const dy = endPoint.y - startPoint.y;
+    bool widestIsX;
+    float slope;
+    float startW, startN;
+    float endW;
+    float stepW; // +1.0/-1.0
+    if (std::abs(dx) > std::abs(dy))
+    {
+        widestIsX = true;
+        slope = static_cast<float>(dy) / static_cast<float>(dx);
+        startW = static_cast<float>(startPoint.x);
+        startN = static_cast<float>(startPoint.y);
+        endW = static_cast<float>(endPoint.x);
+        stepW = static_cast<float>(dx) / static_cast<float>(std::abs(dx));
+    }
+    else
+    {
+        widestIsX = false;
+        slope = static_cast<float>(dx) / static_cast<float>(dy);
+        startW = static_cast<float>(startPoint.y);
+        startN = static_cast<float>(startPoint.x);
+        endW = static_cast<float>(endPoint.y);
+        stepW = static_cast<float>(dy) / static_cast<float>(std::abs(dy));
+    }
+
+    float curW = startW;
+    float curN = startN;
+
+    auto const makePosition = [&]() ->_IntegralCoordinates<_TIntegralTag>
+    {
+        vec2f newPosition;
+        if (widestIsX)
+        {
+            newPosition = vec2f(curW, curN);
+        }
+        else
+        {
+            newPosition = vec2f(curN, curW);
+        }
+
+        return _IntegralCoordinates<_TIntegralTag>(
+            static_cast<typename _IntegralCoordinates<_TIntegralTag>::integral_type>(std::round(newPosition.x)),
+            static_cast<typename _IntegralCoordinates<_TIntegralTag>::integral_type>(std::round(newPosition.y)));
+    };
+
+    //
+    // Visit all other points
+    //
+
+    _IntegralCoordinates<_TIntegralTag> oldPosition = startPoint;
+
+    while (true)
+    {
+        curW += stepW;
+
+        if constexpr (TType == IntegralLineType::WithAdjacentSteps)
+        {
+            auto const newPosition = makePosition();
+            if (newPosition != oldPosition)
+            {
+                visitor(newPosition);
+
+                oldPosition = newPosition;
+            }
+        }
+
+        curN += slope * stepW;
+
+        auto const newPosition = makePosition();
+        if (newPosition != oldPosition)
+        {
+            visitor(newPosition);
+
+            oldPosition = newPosition;
+        }
+
+        // Check if done
+        if (fabs(endW - curW) <= 0.5f)
+        {
+            // Reached destination
+            break;
+        }
+    }
+}
